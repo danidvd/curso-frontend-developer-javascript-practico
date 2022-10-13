88461261c235cb7cd44ef807e47801f626890ecf
Daniel David <danidvd821@gmail.com>
2022-10-12 22:45:10 -0500

    Iniciamos con la parte del producto detalles

---
+++ main.js
@@ -0,0 +1,107 @@
+// Menu Desktop
+const menuemail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+
+//Menu Mobile
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+//Carrito de compra Details
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
+
+const cardsContainer= document.querySelector(".cards-container");
+
+
+// Eventos 
+menuemail.addEventListener("click",toggleDesktopMenu);
+menuHamIcon.addEventListener("click",toggleMobileMenu);
+menuCarritoIcon.addEventListener("click",toggleCarritoAside)
+
+//Funciones
+function toggleDesktopMenu(){
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
+
+    if(!isAsideClosed){
+        shoppingCartContainer.classList.add("inactive")
+    }
+
+    desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    const isAsideClosed = shoppingCartContainer.classList.contains("inactive");
+
+    if (!isAsideClosed){
+        shoppingCartContainer.classList.add("inactive");
+    }
+
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add("inactive");
+    }
+
+    shoppingCartContainer.classList.toggle("inactive");
+}
+
+const productList = [];
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "TV",
+    price: 520,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Laptop",
+    price: 600,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+
+function renderProducts(arr){
+    for (product of arr) {
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+    
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src",product.image);
+    
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv = document.createElement("div");
+    
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$ " + product.price;
+    
+        const productName = document.createElement("p");
+        productName.innerText = product.name;
+    
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+    
+        const productInfoFigure = document.createElement("figure");
+        const productImgCart = document.createElement("img");
+        productImgCart.setAttribute("src","./icons/bt_add_to_cart.svg");
+    
+        productInfoFigure.appendChild(productImgCart);
+    
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList);
